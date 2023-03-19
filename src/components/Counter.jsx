3f70dd31405887310a3ba44ef2a738e00f3aadd2
Hassaam Mehtab <h455a4mmehtab@gmail.com>
2023-03-19 18:08:29 +0500

    Project Completed

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import '../styles/counter.css'
+import React, { useState } from "react";
+import "../styles/counter.css";
 
 const Counter = () => {
-	return <div>The counter</div>;
+	const [count, setCount] = useState(0);
+
+	const handleOnclickreset = () => {
+		setCount(0);
+	};
+	const handleOnClickPlus = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div className="main">
+			<div className="body">
+				<span className="display">{count}</span>
+				<div>
+					<button onClick={handleOnclickreset}>Reset</button>
+					<button onClick={handleOnClickPlus}>Increase</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Counter;
